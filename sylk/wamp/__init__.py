5a6a4ddc569674b7f3d517386cc0ad2a3ba8f43d
tarun <agumbe22@gmail.com>
2018-03-08 14:33:26 +0530

    Test

--- sylk/wamp/__init__.py
+++ sylk/wamp/__init__.py
@@ -32,10 +32,10 @@ def joined(session, details):
             notification_data = NotificationData(username=data['username'], \
                                                   status=data['status'], wamp_session_id=data['wamp_session_id'], user_id=data['user_id'])
             notification_center.post_notification('CalltakerStatus', session, notification_data)
-            data = {
+            out = {
                 'command' : 'status_updated'
             }
-            yield session.publish(u'com.emergent.calltakers', data)
+            yield session.publish(u'com.emergent.calltakers', out)
             log.info("sent status_updated")
 
     def on_session_leave(data):
