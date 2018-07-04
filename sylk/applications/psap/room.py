9906bfbfec90a4021bbaf0158ae4b0c1c336ed4a
tarun <agumbe22@gmail.com>
2018-07-03 17:55:13 -0700

    another fix to try

--- sylk/applications/psap/room.py
+++ sylk/applications/psap/room.py
@@ -268,7 +268,7 @@ class Room(object):
         self.duration_timer.start(1)
         log.info("inside room start 11")
 
-    @run_in_twisted_thread
+    @run_in_green_thread
     def start_recorder(self):
         log.info("room start_recorder")
         if self.recorder is None and self.started:
@@ -281,7 +281,7 @@ class Room(object):
             log.info("room start_recorder done")
             self.audio_conference.bridge.add(self.recorder)
 
-    @run_in_twisted_thread
+    @run_in_green_thread
     def stop_recorder(self):
         log.info("room stop_recorder")
         if self.recorder != None:
