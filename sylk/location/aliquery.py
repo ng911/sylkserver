02d8bd3491b53e816f123dd557dc28f92583f71c
tarun <agumbe22@gmail.com>
2018-07-22 00:16:25 -0600

    testing

--- sylk/location/aliquery.py
+++ sylk/location/aliquery.py
@@ -491,7 +491,8 @@ def on_timeout(id):
         del g_ali_requests[id]
         for protocol in protocols:
             protocol.cancel_ali_request(id)
-        my_d.errback(AliRequestTimeout(room_number, "request timedout"))
+        #my_d.errback(AliRequestTimeout(room_number, "request timedout"))
+        my_d.errback("request timedout")
 
 def process_ali_result(result):
     log.info("aliquery process_ali_result %r", result)
@@ -505,7 +506,9 @@ def process_ali_result(result):
             if _factory != factory:
                 factory.cancel_ali_request(id)
         log.info("aliquery do my_d.callback")
-        my_d.callback((room_number, number, ali_format, ali_result, ali_result_civic_xml, ali_data))
+        #my_d.callback((room_number, number, ali_format, ali_result, ali_result_civic_xml, ali_data))
+        # just for testing
+        my_d.errback("request timedout")
 
 
 def send_ali_request(room_number, number, ali_format):
