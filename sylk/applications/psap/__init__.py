cfef90d01ffa0ddae03200d21d7f5a064cead545
tarun <agumbe22@gmail.com>
2018-12-13 19:54:50 -0600

    Test

--- sylk/applications/psap/__init__.py
+++ sylk/applications/psap/__init__.py
@@ -16,7 +16,6 @@ from sipsimple.core import Engine, SIPCoreError, SIPURI, ToHeader, FromHeader, H
 from sipsimple.lookup import DNSLookup
 from sipsimple.configuration.settings import SIPSimpleSettings
 from sipsimple.configuration.settings import SIPSimpleSettings
-from sipsimple.audio import wave_tty_tone_test
 from sipsimple.session import IllegalStateError
 from sylk.session import Session
 from sylk.accounts import get_user_account
@@ -175,7 +174,6 @@ class PSAPApplication(SylkApplication):
         call_data.CallData()
         conf_data.ConferenceData()
         self._rooms = {}
-        wave_tty_tone_test()
 
     def init_observers(self):
         log.info("ConferenceData init_observers")
