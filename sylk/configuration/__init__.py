01bf8ea1bc23d497621d806a3636c376cdb937a0
tarun <agumbe22@gmail.com>
2018-12-14 07:55:56 -0600

    Test to change sample rate

--- sylk/configuration/__init__.py
+++ sylk/configuration/__init__.py
@@ -87,7 +87,7 @@ class RTPConfig(ConfigSection):
     port_range = ConfigSetting(type=PortRange, value=PortRange('50000:50500'))
     srtp_encryption = ConfigSetting(type=SRTPEncryption, value='opportunistic')
     timeout = ConfigSetting(type=NonNegativeInteger, value=30)
-    sample_rate = ConfigSetting(type=SampleRate, value=48000)
+    sample_rate = ConfigSetting(type=SampleRate, value=8000)
 
 
 class WebServerConfig(ConfigSection):
