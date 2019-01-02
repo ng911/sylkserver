2c6339ebf4a32c442defdb63335097a4a9f161a6
tarun <agumbe22@gmail.com>
2019-01-01 22:23:03 -0800

    msrp testing

--- sylk/api/calls.py
+++ sylk/api/calls.py
@@ -506,6 +506,7 @@ def conference_msrp_text(room_number):
             # should return date, call, type, caller, callback, location, long, lat, notes, status
             message_data = get_json_from_db_obj(db_obj)
             messages.append(message_data)
+        log.debug("/conference/msrp/text messages is %r", messages)
 
         return { 'success': True, 'messages' : messages, 'room_number' : room_number}
     except Exception as e:
