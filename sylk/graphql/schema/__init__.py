895249f85e8ad3e58e9fad097722993d2971bd54
tarun <agumbe22@gmail.com>
2020-04-21 16:40:58 +0530

    Added support for speed dial groups

--- sylk/graphql/schema/__init__.py
+++ sylk/graphql/schema/__init__.py
@@ -5,22 +5,19 @@ from ..fields import OrderedMongoengineConnectionField
 from .user import UserNode
 from .psap import PsapNode
 from .queue import QueueNode
-from .speed_dial import SpeedDialNode
+from .speed_dial import SpeedDialNode, SpeedDialGroupNode
 from .calls import ConferenceNode
 
 
 class Query(graphene.ObjectType):
     node = Node.Field()
 
-    name = graphene.String()
     all_users = OrderedMongoengineConnectionField(UserNode)
     all_psaps = OrderedMongoengineConnectionField(PsapNode)
     all_queues = OrderedMongoengineConnectionField(QueueNode)
     all_speed_dials = OrderedMongoengineConnectionField(SpeedDialNode)
+    all_speed_dial_groups = OrderedMongoengineConnectionField(SpeedDialGroupNode)
     all_conferences = OrderedMongoengineConnectionField(ConferenceNode)
-
-    def resolve_name(parent, info, **args):
-        return "Hello World"
 
 
 graphql_schema = graphene.Schema(query=Query, types=[])
--- sylk/graphql/schema/speed_dial.py
+++ sylk/graphql/schema/speed_dial.py
@@ -5,10 +5,30 @@ from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
 
 from ..utiils import update_params_with_args
 from ...db.schema import SpeedDial as SpeedDialModel
+from ...db.schema import SpeedDialGroup as SpeedDialGroupModel
+from ...db.schema import User as UserModel
 
 
 class SpeedDialNode(MongoengineObjectType):
     class Meta:
         model = SpeedDialModel
         interfaces = (Node,)
+
+    user = MongoengineObjectType(UserModel)
+
+    def resolve_user(parent, info, **args):
+        return UserModel.objects.get(user_id = parent.user_id)
+
+
+class SpeedDialGroupNode(MongoengineObjectType):
+    class Meta:
+        model = SpeedDialGroupModel
+        interfaces = (Node,)
+
+    speed_dials = MongoengineConnectionField(SpeedDialNode)
+
+    def resolve_speed_dials(parent, info, **args):
+        return SpeedDialModel.objects(group_id = parent.group_id)
+
+
 
