f26477c434c8987b43025869eee5284ed287d095
tarun <agumbe22@gmail.com>
2020-07-10 12:10:57 +0530

    Change domain_name to domain in psap schema db call

--- sylk/db/psap.py
+++ sylk/db/psap.py
@@ -1,7 +1,7 @@
 from .schema import Psap
 
 def get_psap_from_domain(domain_name):
-    psapObj = Psap.objects.get(domain_name=domain_name)
+    psapObj = Psap.objects.get(domain=domain_name)
     return str(psapObj.psap_id)
 
 
