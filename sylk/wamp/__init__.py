4b8dd6870c3e409b7f99d9f1a0992b0386cbdadd
tarun <agumbe22@gmail.com>
2020-07-15 16:58:41 +0530

    Added graphql relay update support for py 2 as well

--- sylk/wamp/__init__.py
+++ sylk/wamp/__init__.py
@@ -7,6 +7,7 @@ from .calltaker import publish_update_calltaker_status, publish_update_calltaker
 from .location import publish_update_location_failed, publish_update_location_success
 from .msrp import publish_msrp_message
 from .tty import publish_tty_enabled, publish_tty_updated
+from .graphql import publish_relay_node_add, publish_relay_node_update
 
 __all__ = [
     'start', 'wamp_publish',
@@ -15,7 +16,8 @@ __all__ = [
     'publish_clear_abandoned_call', 'publish_active_call', 'publish_create_call', 'publish_update_calls',
     'publish_update_calltaker_status', 'publish_update_calltakers',
     'publish_update_location_failed', 'publish_update_location_success',
-    'publish_msrp_message', 'publish_tty_enabled', 'publish_tty_updated'
+    'publish_msrp_message', 'publish_tty_enabled', 'publish_tty_updated',
+    'publish_relay_node_update', 'publish_relay_node_add'
 ]
 
 
