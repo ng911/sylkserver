06a5719e7f88fb8e7cd3ecae54bed3f3a0db280f
tarun <agumbe22@gmail.com>
2020-08-22 22:08:50 +0530

    debug videoconnector

--- sylk/applications/psap/__init__.py
+++ sylk/applications/psap/__init__.py
@@ -2061,8 +2061,8 @@ class PSAPApplication(SylkApplication):
             log.info("do connect")
 
             log.info("do create caller_video_connector")
-            log.info("do create caller_video_connector producer port %r", calltaker_local.producer_port)
-            log.info("do create caller_video_connector consumer port %r", caller_remote.consumer_port)
+            log.info("do create caller_video_connector producer port %r", caller_remote.producer_port)
+            log.info("do create caller_video_connector consumer port %r", calltaker_local.consumer_port)
             caller_video_connector = VideoConnector(caller_remote, calltaker_local)
             log.info("caller_video_connector created")
             '''
