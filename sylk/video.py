f69e60ab2c4d5807eda2820c8b0957e570e7ada3
tarun <agumbe22@gmail.com>
2020-08-26 23:33:41 +0530

    fix for video conf

--- sylk/video.py
+++ sylk/video.py
@@ -4,9 +4,9 @@ class VideoConferenceSession(object):
     def __init__(self, video_mixer, video_stream):
         log.info("inside VideoConferenceSession __init__ %r", video_stream)
         self.video_mixer = video_mixer
-        self.consumer_port = video_stream._transport.remote_video.consumer_port
+        self.producer_port = video_stream._transport.remote_video.producer_port
         log.info("inside VideoConferenceSession self.consumer_port %r", self.consumer_port)
-        self.producer_port = video_stream._transport.local_video.producer_port
+        self.consumer_port = video_stream._transport.local_video.consumer_port
         log.info("inside VideoConferenceSession self.consumer_port %r", self.producer_port)
         self.source_slot = video_mixer._add_port(self.producer_port)
         log.info("inside VideoConferenceSession self.source_slot %r", self.source_slot)
