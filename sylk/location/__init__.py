1d4f2769310a2132f95bed4c8d864fd19614e379
tarun <agumbe22@gmail.com>
2020-09-15 20:55:51 +0530

    remove lat/long truncation, do it on the client instead

--- sylk/location/__init__.py
+++ sylk/location/__init__.py
@@ -296,11 +296,6 @@ def aliRebid(self):
     self.rebidPending = False
 '''
 
-import math
-def truncate(number, digits):
-    stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
-
 def derefLocation(room_number, psap_id, geolocation, callerName):
     class Options(object):
         pass
@@ -344,12 +339,8 @@ def derefLocation(room_number, psap_id, geolocation, callerName):
             if longitude == None or longitude == "":
                 longitude = ali_long
             if latitude != None and latitude != "":
-                latitude = float(latitude)
-                latitude = truncate(latitude, 8)
-                location_db_obj.latitude = latitude
+                location_db_obj.latitude = float(latitude)
             if longitude != None and longitude != "":
-                longitude = float(longitude)
-                longitude = truncate(longitude, 8)
                 location_db_obj.longitude = float(longitude)
             if radius != None and radius != "":
                 location_db_obj.radius = float(radius)
