31d7b4b948566c94bd61cdff137f06354d3c520e
tarun <agumbe22@gmail.com>
2020-09-29 15:47:49 +0530

    added support for file_pomd react lib in resource

--- sylk/api/resource.py
+++ sylk/api/resource.py
@@ -1,9 +1,12 @@
 import logging
 import os
+import bson
+import errno
 
-from flask import Blueprint, current_app, send_from_directory
+from flask import Blueprint, current_app, send_from_directory, request, abort
 from flask_cors import CORS
 from flask_restful import reqparse
+from werkzeug.utils import secure_filename
 
 from ..db.queue import add_calltaker_to_queue, add_queue, get_queue_details, get_queue_members, \
     remove_calltaker_from_queue, remove_queue, edit_queue, get_queues
@@ -30,4 +33,35 @@ def get_resource(psap_id, sub_folder, file_name):
     subpath = os.path.join(psap_id, sub_folder)
     file_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], subpath)
     return  send_from_directory(file_dir, file_name)
+
+@resource.route('/process', methods=['POST'])
+def add_resource():
+    """ GET a resource
+        ---
+        get:
+            summary: get a resource.
+            description: get a resource given psap id and resoure id
+    """
+    resource_id = str(bson.ObjectId())
+
+    if 'file' not in request.files:
+        log.error("add_resource no fille in request")
+        log.error("add_resource request.files is %r, %s, length is %d", request.files, str(request.files), len(request.files))
+        for name in request.files:
+            log.info("found %r, %s", name, str(name))
+        abort(400, 'No file part')
+
+    file = request.files['file']
+    if file.filename == '':
+        abort(400, 'No selected file')
+
+    filename = secure_filename(file.filename)
+    file_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], resource_id)
+    try:
+        os.makedirs(file_dir)
+    except OSError as exc:  # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            abort(400, 'error in creating directory ')
+    file.save(os.path.join(file_dir, filename))
+    return resource_id
 
