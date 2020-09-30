f33507bf2de41e1ea8ebccbb70bb238ff19348ab
tarun <agumbe22@gmail.com>
2020-09-30 13:36:16 +0530

    fix for file upload in /resource

--- sylk/api/resource.py
+++ sylk/api/resource.py
@@ -46,24 +46,25 @@ def add_resource():
     log.info("request is %r", request)
     log.info("request.files is %r", request.files)
 
-    if 'file' not in request.files:
+    if 'files' not in request.files:
         log.error("add_resource no fille in request")
         log.error("add_resource request.files is %r, %s, length is %d", request.files, str(request.files), len(request.files))
         for name in request.files:
             log.info("found %r, %s", name, str(name))
         abort(400, 'No file part')
 
-    file = request.files['file']
-    if file.filename == '':
+    files = request.files['files']
+    log.info("files is %r", files)
+    if files.filename == '':
         abort(400, 'No selected file')
 
-    filename = secure_filename(file.filename)
+    filename = secure_filename(files.filename)
     file_dir = os.path.join(current_app.config['UPLOAD_FOLDER'], resource_id)
     try:
         os.makedirs(file_dir)
     except OSError as exc:  # Guard against race condition
         if exc.errno != errno.EEXIST:
             abort(400, 'error in creating directory ')
-    file.save(os.path.join(file_dir, filename))
+    files.save(os.path.join(file_dir, filename))
     return resource_id
 
